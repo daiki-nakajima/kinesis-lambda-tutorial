59ba3c8935c2b9a13f0f5d739dd5e8ecef29060f
daiki.nakajima <stovepipe531@gmail.com>
2018-09-12 00:28:56 +0900

    s3 upload function

--- ProcessKinesisRecords.js
+++ ProcessKinesisRecords.js
@@ -1,4 +1,10 @@
+'use strict'
+
 console.log('Loading function');
+
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var recCnt = 0;
 
 exports.handler = function(event, context, callback) {
     console.log(JSON.stringify(event, null, 2));
@@ -6,6 +12,17 @@ exports.handler = function(event, context, callback) {
         // Kinesis data is base64 encoded so decode here
         var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
         console.log('Decoded payload:', payload);
+        
+        recCnt++;
+        var params = {
+            Body: payload, 
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: "rec" + recCnt + ".txt"
+        };
+        s3.putObject(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+        });
     });
     callback(null, "message");
 };
